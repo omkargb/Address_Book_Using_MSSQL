88adec4037b4c76cdad0df91381a45762a348eea
omkargb <omkargb107@gmail.com>
2021-09-01 01:04:41 +0530

    [omkargb] Add : Insert and select query on contact table executed

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,5 +1,7 @@
 create database AddressBookDB
 
+use AddressBookDB
+
 create table PersonContact(
 Id int identity(1,1) NOT NULL PRIMARY KEY,
 FirstName varchar(16) ,
@@ -7,7 +9,16 @@ LastName varchar(16),
 Address varchar(24),
 City varchar(16),
 State varchar(16),
-ZipCode varchar(16),
-PhoneNumber varchar(8),
-EmailId varchar(8)
+ZipCode varchar(6),
+PhoneNumber varchar(16),
+EmailId varchar(24)
 );
+
+Insert into PersonContact(FirstName,LastName,City,State,ZipCode,PhoneNumber,EmailId) values 
+('Omkar','B','Mumbai','MH','400091','9999888877','omkarb@mail.com'),
+('Ramesh','M','Sansad Marg','Delhi','110001','9988558899','rameshm@mail.com'),
+('Suresh','K','Swargate','MH','411042','8877995544','sureshk@mail.com'),
+('Riya','D','Chennai','Tamil Nadu','600005','7744112233','riyad@mail.com'),
+('Neha','G','Kolkata','West Bangal','700073','7775553330','nehag@mail.com');
+
+select * from PersonContact;
